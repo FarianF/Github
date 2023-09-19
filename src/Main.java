f3212884449ec954c0ca6802cd7f93d7c09af432
BT_4N2_12 <farianf2@nycstudents.net>
2023-09-19 13:05:19 -0400

    Added line for favorite food

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@
 public class Main {
     public static void main(String[] args) {
       System.out.println("Farian Faruqee");
-        System.out.println("My favorite movie is rush hour 1 to 3");
+      System.out.println("My favorite movie is rush hour 1 to 3");
+      System.out.println("My favorite food is gyro");
         }
     }
